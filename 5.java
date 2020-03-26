3455c23bea643d976787953c30e270d7fff9d5e3
YangSu03 <yang.su@student.tdsb.on.ca>
2020-03-26 15:51:19 +0000

    5

--- 5.java
+++ 5.java
@@ -1,13 +1,18 @@
 import java.util.*;
 class Five{
   public static void main(String[] args) {
-  String word = "ARRAY";
-  char [] arr  = word.toCharArray();
-  for (int i = 0; i < word.length(); i++ ) {
-    for (int j = )
-    System.out.print(arr [i]);
-
-  }
+  String str = "ARRAY";
+  char[] matrix = str.toCharArray();
+  for (int i = 0; i < matrix.length; i++) {
+    for (int j = matrix.length - i; j > i; j++) {
+    if (j == i + 1) {
+      System.out.println(matrix[j]);
+  } else {
+      System.out.print(matrix[j]);
+    }
+    }
+    } 
   }
 }
+
   
